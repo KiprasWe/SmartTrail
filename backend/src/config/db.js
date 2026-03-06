42bfc8b2249b31ca63123bb9d068382e14745e34
KiprasWe <veberiskipras123@gmail.com>
2026-03-06 19:44:40 +0200

    initail commit expressjs auth and expo default project

---
+++ backend/src/config/db.js
@@ -0,0 +1,28 @@
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+
+const prisma = new PrismaClient({
+  log:
+    process.env.NODE_ENV === "development"
+      ? ["query", "error", "warn"]
+      : ["error"],
+  adapter,
+});
+
+const connectDB = async () => {
+  try {
+    await prisma.$connect();
+    console.log("DB Connected via Prisma");
+  } catch (error) {
+    console.error("DB Connection Error:", error);
+    process.exit(1);
+  }
+};
+
+const disconnectDB = async () => {
+  await prisma.$disconnect();
+};
+
+export { prisma, connectDB, disconnectDB };
---
+++ backend/src/controllers/authController.js
@@ -0,0 +1,86 @@
+import { prisma } from "../config/db.js";
+import bcrypt from "bcryptjs";
+import { generateToken } from "../utils/generateToken.js";
+
+const signup = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  const userExists = await prisma.user.findUnique({ where: { email } });
+
+  if (userExists) {
+    return res
+      .status(400)
+      .json({ error: "User already exists with this email" });
+  }
+
+  // Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // Create user
+  const user = await prisma.user.create({
+    data: {
+      username,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // Generate token
+  const token = generateToken(user.id, res);
+
+  res.status(201).json({
+    status: "success",
+    data: {
+      user: {
+        id: user.id,
+        username: username,
+        email: email,
+      },
+      token,
+    },
+  });
+};
+
+const signin = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await prisma.user.findUnique({ where: { email } });
+
+  if (!user) {
+    return res.status(401).json({ error: "Invalid email or password" });
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordValid) {
+    return res.status(401).json({ error: "Invalid email or password" });
+  }
+
+  // Generate token
+  const token = generateToken(user.id, res);
+
+  res.status(201).json({
+    status: "success",
+    data: {
+      user: {
+        id: user.id,
+        email: email,
+      },
+      token,
+    },
+  });
+};
+
+const signout = async (req, res) => {
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+  res.status(200).json({
+    status: "success",
+    message: "Logged out successfully",
+  });
+};
+
+export { signup, signin, signout };
